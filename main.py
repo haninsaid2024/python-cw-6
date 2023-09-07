3970574f979b0031463fa824ca893673c7459595
haninsaid2024 <haninfouda077@gmail.com>
2023-09-07 15:47:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-# write your code here
+person = {
+    "name": "haneen",
+    "age": 15,
+    "hobbies":['reeding' , 'cooking' , 'playing_games']   
+
+}
+
+print(person["name"])
+print(person['age'])
+person["age"] = 16 
+print(person['age'])
+person['country']='Egypet'
+print(person)
+person["hobbies"].append("skating")
+print(person)
+len(person)
+print(len(person))
+
+def check_hobbies(person):
+  if len(person)["hobiies"] > 3:
+     print('wow you are ama')
+  else:  
+      print('Falus')
+check_hobbies(person)
+
+
+
+
+
+
+
+
+
